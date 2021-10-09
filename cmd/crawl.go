33d29e5ee5f4f923ac07883b6a462ed019ce701b
Andy Nogueira <me@andynogueira.dev>
2021-10-09 08:19:57 -0400

    Added list flag to the crawl command #9

--- cmd/crawl.go
+++ cmd/crawl.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
-	"os"
-
+	"flag"
+	"fmt"
 	"github.com/spf13/cobra"
 )
+
+var list string
 
 // crawlCmd represents the crawl command
 var crawlCmd = &cobra.Command{
@@ -13,12 +15,17 @@ var crawlCmd = &cobra.Command{
 	Long:  `Crawl repositories from a pre-defined list of Cosmos SDK projects.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		_ = cmd.Help()
-		os.Exit(0)
+		flag.Parse()
+		if list == "" {
+			fmt.Println("Please specify the repository list filename")
+		} else {
+			fmt.Printf("Crawling the repositories in %s\n", list)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(crawlCmd)
-	crawlCmd.PersistentFlags().BoolVarP(&jsonOutput, "json", "j", false, "Output results to JSON")
-	crawlCmd.PersistentFlags().StringVarP(&sortBy, "repository list", "l", "", "List of URLs of GitHub Cosmos SDK repositories to crawl")
+	crawlCmd.PersistentFlags().BoolVarP(&jsonOutput, "json", "j", false, "output results to JSON")
+	crawlCmd.PersistentFlags().StringVarP(&list, "list", "l", "", "path of the file with the list of Github repositories to crawl")
 }
